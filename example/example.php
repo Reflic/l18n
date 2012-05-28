3fb43d64ee2d4fbf080d133013eb15cf5d59bf91
Kevin <reflic@notesafe.de>
2012-05-28 21:30:45 +0200

    Added one example.

--- example/example.php
+++ example/example.php
@@ -7,9 +7,10 @@ l18n::get_country(); #get the current country (OUTPUT: en)
 l18n::get_language(); #get the current language (OUTPUT: GB)
 l18n::get_langcode(); #get the current langcode (OUTPUT: en_GB)
 
-__(form.name, 'Name'); #get's the translation for the key form.name (OUTPUT: Your name)
-l18n::line(form.name, 'Name'); #equivalent to the example above
-__(index.slogan, 'Sample Slogan') #get's the translation for the key index.slogan (OUTPUT: The good system for localization.)
+__('form.name', 'Name'); #get's the translation for the key form.name (OUTPUT: Your name)
+l18n::line('form.name', 'Name'); #equivalent to the example above
+__('index.slogan', 'Sample Slogan') #get's the translation for the key index.slogan (OUTPUT: The good system for localization.)
 
-
+__('abc.test', 'abc test')#get's the translation for the key abc.test. Cause abc.test has no translation the default text will be 
+						  #given out and an log entry would be written. (OUTPUT: abc test)
 ?>
