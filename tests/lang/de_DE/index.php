1e9ca5b89325487bc74fcddebc20fcd51eb7114c
Reflic <reflic@notesafe.de>
2013-08-29 23:52:20 +0200

    Added test lang files to tests/ folder.

---
+++ tests/lang/de_DE/index.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'title' => 'Titel',
+	'slogan' => 'Das gute System zur Lokalisation.',
+	'user' => 'User',
+	'last_page' => 'Letzte Seite',
+	'no_rights' => 'Sie haben nicht die erforderlichen Rechte um dieses Seite zu besuchen.'
+	);
+
+?>
---
+++ tests/lang/en_GB/form.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'name' => 'Your name',
+	'address' => 'Your adress',
+	'comment' => 'Your comment',
+	);
+
+?>
